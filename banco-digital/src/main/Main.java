2c63a7a0f986714730fdd61d1c21c155ded6cedc
Arthur Cristian Reis <88160811+ArthurCristian@users.noreply.github.com>
2022-05-21 13:11:46 -0300

    adicionando documentos via upload

---
+++ banco-digital/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import conta.Conta;
+import conta.ContaCorrente;
+import conta.ContaPoupanca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
